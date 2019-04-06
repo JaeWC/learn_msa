3c316b141422785c56ec1dbf6cf99aeac02d538a
Jae Choi <j.thechois@gmail.com>
2019-04-07 00:33:21 +0900

    add body-parser

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
 let express = require('express');
+let body = require('body-parser'); // 여러 본문 유형과 압축을 지원하는 body-parser module
+let route = express.Router();
 let app = express();
 let stack = [];
 
-app.post('/stack', (req, res, next) => {
-  let buffer = '';
+app.use(body.text({ type: '*/*' }));
 
-  req.on('data', data => {
-    buffer += data;
-  });
-  req.on('end', () => {
-    stack.push(buffer);
-    return next();
-  });
-});
+route.post('/stack', (req, res, next) => {
+  stack.push(req.body);
 
-app.delete('/stack', (req, res, next) => {
-  stack.pop();
   return next();
 });
 
-app.get('/stack/:index', (req, res) => {
+route.delete('/stack', (req, res, next) => {
+  stack.pop();
+
+  return next();
+});
+
+route.get('/stack/:index', (req, res) => {
   if (req.params.index >= 0 && req.params.index < stack.length) {
     return res.end('' + stack[req.params.index]);
   }
   res.status(404).end();
 });
 
-app.use('/stack', (req, res) => {
-  res.send(stack);
-});
+app.use('/stack', route);
 
 app.listen(3000);
